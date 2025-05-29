eeb4bef36ee819924c5a07b655176a82dea37458
KenLZY <112811713+KenLZY@users.noreply.github.com>
2025-05-29 18:31:17 +0800

    feat(ce2): Updated Task 1

--- ce2/ce2_q1/routes/dept.js
+++ ce2/ce2_q1/routes/dept.js
@@ -5,8 +5,15 @@ var router = express.Router();
 
 
 router.get('/add/:code', async function(req, res, next) {
-    
-    res.send(`TODO`); // TODO: Fixme
+    // Step 1: retrieve the parameter :code
+    const msg = req.params.code;
+    // Step 2: create the document to be put into the collection
+    const document = new deptmodel.Dept(msg);
+    // Step 3: insert it into the collection
+    await deptmodel.insertMany([document]);
+    // this line will return all documents within the collection deptmodel
+    const allDepartments = await deptmodel.all()
+    res.send(`${JSON.stringify(allDepartments)}`); // TODO: Fixme
 });
 
 
@@ -14,12 +21,15 @@ router.get('/add/:code', async function(req, res, next) {
 /* GET dept listing. */
 
 router.get('/all/', async function(req, res, next) {
-    res.send(`TODO`); // TODO: Fixme
+    const allDepartments = await deptmodel.all();
+    res.send(`${JSON.stringify(allDepartments)}`); // TODO: Fixme
 });
 
 
 router.get('/all/withstaff/', async function(req, res, next) {
-    res.send(`TODO`); // TODO: Fixme
+    // filter staff with a department not NILL
+    const staffWithDept = await staffmodel.find("dept");
+    res.send(`${JSON.stringify(staffWithDept)}`); // TODO: Fixme
 })
 
 
--- ce2/ce2_q1/routes/staff.js
+++ ce2/ce2_q1/routes/staff.js
@@ -6,13 +6,24 @@ var router = express.Router();
 
 /* insert a staff, should have used POST instead of GET */
 router.get('/add/:id/:name/:code', async function(req, res, next) {
-    res.send(`TODO`); // TODO: Fixme
+    // retrieve the parameters first
+    const id = req.params.id;
+    const name = req.params.name;
+    const code = req.params.code;
+    // create a new document to be inserted into the staff collection
+    const document = new staffmodel.Staff(id, name, code);
+    // insert the document into the collection
+    await staffmodel.insertMany([document])
+    // now, return in JSON, the documents in the collection
+    const allStaff = await staffmodel.all();
+    res.send(`${JSON.stringify(allStaff)}`)
 });
 
 /* GET staff listing. */
 
 router.get('/all/', async function(req, res, next) {
-    res.send(`TODO`); // TODO: Fixme
+    const allStaff = await staffmodel.all();
+    res.send(`${JSON.stringify(allStaff)}`);
 });
 
 
