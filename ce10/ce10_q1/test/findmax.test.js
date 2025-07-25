dd49538735f3fda0c023ce587b7667c0a5cf9f7a
KenLZY <112811713+KenLZY@users.noreply.github.com>
2025-07-25 11:42:54 +0800

    Add comprehensive tests for findmax function
    Implemented passing, failing, and error-throwing test cases for the findmax function. Added additional tests for arrays containing null and NaN values to improve coverage.

--- ce10/ce10_q1/test/findmax.test.js
+++ ce10/ce10_q1/test/findmax.test.js
@@ -1,16 +1,22 @@
-const findmax = require('../src/findmax.js');
+const findmax = require("../src/findmax.js");
 
-describe('testing findmax', () => {
-    // TODO: a test that results in failure (do not compute maximum) it should fail
-    test('a test that fails', () => {
-        
-    })
-    // TODO: a test that results in error, it should throw an error that the test won't catch
-    test('a test that throws error', () => {
-
-    })
-    // TODO: a test that results in pass
-    test('a test that passes', () => {
-
-    })
+describe("testing findmax", () => {
+  // TODO: a test that results in failure (do not compute maximum) it should fail
+  test("a test that fails", () => {
+    expect(findmax([1, 2, 3])).toBe(3);
+  });
+  // TODO: a test that results in error, it should throw an error that the test won't catch
+  test("a test that throws error", () => {
+    expect(() => findmax([])).toThrow();
+  });
+  // TODO: a test that results in pass
+  test("a test that passes", () => {
+    expect(findmax([1, 9, 3])).toBe(9);
+  });
+  test("a test with null values", () => {
+    expect(findmax([null, null])).toBe(null);
+  });
+  test("a test with NaN values", () => {
+    expect(findmax([NaN, NaN, 2])).toBe(2);
+  });
 });
